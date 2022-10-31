b0016840a7537a810a0d64ab7f05e96a04aab686
GauravGithubbox <76477015+GauravGithubbox@users.noreply.github.com>
2022-10-31 18:58:59 +0530

    Add files via upload

---
+++ CodeInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.codeinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInsightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInsightsApplication.class, args);
+    }
+
+}
